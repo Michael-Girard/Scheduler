1f6a8204828641790f790b27b740fdec0af8b41c
Michael Girard <michael-girard@outlook.com>
2017-12-31 00:59:27 -0500

    Update TestScheduler.java
    Now generates NUMBER_OF_APPOINTMENTS appointments with randomized dates and times from the current year.

--- TestScheduler.java
+++ TestScheduler.java
@@ -1,19 +1,40 @@
-/**
- * @author Michael Girard
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package com.github.michael_girard.scheduler;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
 import java.time.temporal.ChronoUnit;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ *
+ * @author Michael
+ */
 public class TestScheduler extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("TestScheduler");
+        final int NUMBER_OF_APPOINTMENTS = 100;
+        final int CURRENT_YEAR = LocalDate.now().getYear();
         Scheduler scheduler = new Scheduler(ChronoUnit.MONTHS);
-        scheduler.showOnStage(primaryStage);
+        
+        for (int i = 0; i < NUMBER_OF_APPOINTMENTS; i++){
+            LocalTime randomTime = LocalTime.of((int)(Math.random() * 22), 1);
+            scheduler.addEntry(new SchedulerEntry(
+                LocalDate.of(CURRENT_YEAR, (int)(Math.random() * 11 + 1), (int)(Math.random() * 27 + 1)),
+                randomTime,
+                randomTime.plusHours(1),
+                "Random Fun",
+                "qwerty"
+            ));       
+        }
+        scheduler.showOnStage();
     }
 
     /**
